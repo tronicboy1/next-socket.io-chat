5a5bdba158e4f397b22bda034c85c31bee69d42a
inoueaus <87982396+inoueaus@users.noreply.github.com>
2021-11-23 06:45:34 +0900

    socket io running

--- pages/chat/[roomId].tsx
+++ pages/chat/[roomId].tsx
@@ -1,16 +1,24 @@
 import type { NextPage } from "next";
-//import io from "socket.io-client";
+import io from "socket.io-client";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Card from "../../components/UI/Card";
 import Button from "../../components/UI/Button";
 
 const ChatRoom: NextPage = () => {
-  const [socket, setSocket] = useState<{} | null>(null);
+  const [socket, _] = useState(() => io());
   const router = useRouter();
   const roomId = router.query["roomId"];
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("socket connected");
+      socket.emit("connection", { data: "test!" });
+    }),
+    socket.on("status", (data) => {
+      console.log(data);
+    })
+  }, []);
 
   const buttonClick = () => {};
 
--- server/index.ts
+++ server/index.ts
@@ -1,17 +1,32 @@
-import { createServer } from 'http'
-import { parse } from 'url'
-import next from 'next'
+import { Server as nextServer, createServer } from 'http'
+//import { parse } from 'url'
+import next, { NextApiHandler, NextApiRequest } from 'next'
+import { Server as socketioServer, Socket }from "socket.io";
+import express, { Express, Request, Response } from "express";
 
 const port = parseInt(process.env.PORT || '3000', 10)
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
-const handle = app.getRequestHandler()
+const handle: NextApiHandler = app.getRequestHandler()
 
-app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url!, true)
-    handle(req, res, parsedUrl)
-  }).listen(port)
+app.prepare().then(async () => {
+  const expressApp: Express = express();
+  const server: nextServer = createServer(expressApp);
+  const io: socketioServer = new socketioServer();
+
+  io.attach(server);
+
+  expressApp.get("/socket", async (_: Request, res: Response) => {
+    res.send("hello world");
+  })
+
+  io.on("connection", (socket: Socket) => {
+    console.log("connection");
+    socket.emit("status", "Connected!")
+  })
+
+  expressApp.all("*", (req: NextApiRequest, res: any) => handle(req, res));
+  server.listen(port);
 
   // tslint:disable-next-line:no-console
   console.log(

25cc08ec15857280dee66e1ab74a6b94a79cb54a
inoueaus <87982396+inoueaus@users.noreply.github.com>
2021-11-22 17:43:29 +0900

    custom server

--- pages/chat/[roomId].tsx
+++ pages/chat/[roomId].tsx
@@ -1,32 +1,18 @@
 import type { NextPage } from "next";
-import io from "socket.io-client";
+//import io from "socket.io-client";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Card from "../../components/UI/Card";
 import Button from "../../components/UI/Button";
 
 const ChatRoom: NextPage = () => {
-  const [socket, setSocket] = useState< {} | null>(null);
+  const [socket, setSocket] = useState<{} | null>(null);
   const router = useRouter();
   const roomId = router.query["roomId"];
 
-  useEffect(() => {
-    fetch("/api/chat-socket").finally(() => {
-      const newSocket = io("http://localhost:3000", { path: "/api/chat-socket" });
-      newSocket.on("connect", () => {
-        console.log("client connect");
-        newSocket.emit("message");
-      })
-      newSocket.on("message", (msg) => {
-        console.log(msg);
-      })
-      setSocket(newSocket);
-    })
-  }, [])
+  useEffect(() => {}, []);
 
-  const buttonClick = () => {
-    socket.emit("message");
-  };
+  const buttonClick = () => {};
 
   return (
     <Card>
